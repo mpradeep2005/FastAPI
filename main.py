f8bb648f59cdadff0fc261d54b7a7a7ce2aa40b6
pradeep <pradeep14.3.2005@gmail.com>
2025-09-19 22:14:56 +0530

    get by id "/product/id" used Basemodel in model class

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ app = FastAPI()
 def welcome():
     return "hi,welcome"
 
-products = [product(id=1, product_name="pen", product_description="nice pen", price=500)]
+products = [
+    product(id=1, product_name="pen", product_description="nice pen", price=5),
+    product(id=2, product_name="box", product_description="nice box", price=50),
+    product(id=3, product_name="scale", product_description="nice scale", price=15)
+            ]
 
 @app.get("/products")
 def get_all_products():
     return products
+
+@app.get("/product/{id}")
+def get_productby_id(id:int):
+    return products[id]
--- model.py
+++ model.py
@@ -1,12 +1,6 @@
-
-class product:
+from pydantic import BaseModel
+class product(BaseModel):
     id:int
     product_name:str
     product_description: str
     price:int
-
-    def __init__(self,id:int,product_name:str,product_description:str,price:int):
-        self.id=id
-        self.product_name=product_name
-        self.product_description= product_description
-        self.price=price
